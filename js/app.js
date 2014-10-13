0ef6e8af79a65ef2b1a90ca11f7f6e4a6ad2176b
adamfarver <adamfarver@gmail.com>
2014-10-13 13:53:06 -0400

    Main Fixes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 $(document).ready(function(e) {
-    $(document).keypress(function(e) {
-    if(e.which == 13) {
-        $('ul').append('<li class="item"> $("input").val() </li>');
-    }
-	
+    $(document).keyup(function(e) {
+	    if (e.which == 13) {
+	        $('ul').append('<li class="item">' + $("input").val() + '</li>');
+	    }	
+	});
+	$('#add').on('click', function(){
+			$('ul').append('<li class="item">' + $("input").val() + '</li>');
+	    });
+	$('.item').mouseup(function() {
+		$(this).addClass('done');
+	});
 });
-$(this).mouseup(function() {
-$(this).addClass('done');
-});
-});
+$('.item').on("click",function() {
+	$(this).addClass('.done');
+});x
