86b27920e6dc54b5ea9b00287cc6f2d7646264f9
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2014-10-14 12:54:57 -0700

    Strikethrough works now
    I'll email an explanation of why this works.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function(e) {
     $(document).keyup(function(e) {
 	    if (e.which == 13) {
-	        $('ul').append('<li><button>-</button><p class="item">' + $("input").val() + '</p></li>');
+	        $('ul').append('<li><p class="item">' + $("input").val() + '</p></li>');
 	    }	
 	});
 	$('#add').on('click', function(){
-			$('ul').append('<li><button>-</button><p class="item">' + $("input").val() + '</p></li>');
-	    });
-	$('.item').mouseup(function() {
-		$(this).toggleClass('done');
+		$('ul').append('<li><p class="item">' + $("input").val() + '</p></li>');
+	});
+	
+
+	$('main').on('click', 'li', function() {
+		$(this).find('.item').toggleClass('done');
 	});
 });
+
